a60fae52283d37feed096f96b99796e559743536
Dorothy Yau <dkyyau@gmail.com>
2020-12-21 14:00:03 +0000

    Removed API key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const KEY = 'AIzaSyAHD1vQjSxOf26j_hTu1jcGPAL2m4AZfBU';
+const KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
